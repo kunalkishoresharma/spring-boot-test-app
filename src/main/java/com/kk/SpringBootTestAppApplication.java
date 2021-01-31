5462229b7d1cdb584990f58f0f907ac5a1cc5a86
Kunal <kunalkishoresharma.kk@gmail.com>
2021-01-31 21:08:32 +0530

    story id: 301

--- src/main/java/com/kk/SpringBootTestAppApplication.java
+++ src/main/java/com/kk/SpringBootTestAppApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTestAppApplication {
-
+	private Map<String,Object> cache = new HashMap<String,Object>()
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestAppApplication.class, args);
 		int i = 10;
 	}
-
+	//Story id: 301
+	public void loadDataCache(){
+		//logic
+	}
 }
